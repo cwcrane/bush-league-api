aa4561b68dae75b654f454ae1793a85777bfaffe
cwcrane <ccrane@excelsiorintegrated.com>
2015-12-13 23:27:32 -0500

    adds basic routes to all controllers - untested

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   delete '/logout/:id' => 'auth#logout'
 
   resources :users, except: [:new, :edit]
-
+  resources :locations, except: [:new, :edit]
+  resources :leagues, except: [:new, :edit]
+  resources :teams, except: [:new, :edit]
+  resources :seasons, except: [:new, :edit]
+  resources :rosters, except: [:new, :edit]
+  resources :games, except: [:new, :edit]
+  resources :player_stats, except: [:new, :edit]
   #resources :books, except: [:new, :edit]
 end
